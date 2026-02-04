401ae71c7ac6de3775f8ff45d76620dd1e850741
ganiyu lukman <lganiyu43@gmail.com>
2026-02-04 04:11:00 +0100

    Fix Navbar with Link from react-router-dom and inline styling

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
-    <nav style={{ display: 'flex', justifyContent: 'space-between', padding: '10px', backgroundColor: '#eee' }}>
-      <div style={{ fontWeight: 'bold' }}>My Company</div>
-      <ul style={{ display: 'flex', justifyContent: 'space-around', listStyle: 'none', margin: 0, padding: 0, width: '300px' }}>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/services">Services</a></li>
-        <li><a href="/contact">Contact</a></li>
-      </ul>
+    <nav style={{ display: "flex", justifyContent: "space-between", padding: "10px", backgroundColor: "#eee" }}>
+      <div>My Company</div>
+      <div style={{ display: "flex", justifyContent: "space-around", width: "300px" }}>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/services">Services</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
     </nav>
   );
 }
