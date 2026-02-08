fa9d8e031050b9246d4fd6034c0326b4b590dfca
ganiyu lukman <lganiyu43@gmail.com>
2026-02-08 19:41:57 +0100

    Update App.jsx with routing implementation

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,36 @@
+import { Routes, Route, Link, useParams } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
 
 function App() {
   return (
     <div>
       <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <RecipeList />
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <AddRecipeForm />
+              <RecipeList />
+            </>
+          }
+        />
+        <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
+      </Routes>
     </div>
   );
+}
+
+// Wrapper to extract recipeId from URL
+function RecipeDetailsWrapper() {
+  const { id } = useParams();
+  return <RecipeDetails recipeId={Number(id)} />;
 }
 
 export default App;
