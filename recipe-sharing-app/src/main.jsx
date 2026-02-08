d33d87c5826dd253656386cb3ab9dc91ca199571
ganiyu lukman <lganiyu43@gmail.com>
2026-02-08 19:13:18 +0100

    Enhance Recipe Sharing App with detailed recipe management (edit, delete, view details)

--- recipe-sharing-app/src/main.jsx
+++ recipe-sharing-app/src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
-import './index.css';
+import RecipeDetails from './components/RecipeDetails';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
+
+// Wrapper to extract recipeId from URL
+import { useParams } from 'react-router-dom';
+
+function RecipeDetailsWrapper() {
+  const { id } = useParams();
+  return <RecipeDetails recipeId={Number(id)} />;
+}
