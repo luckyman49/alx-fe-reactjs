130d97f65213df24c7af58cdef949919f32eefbc
ganiyu lukman <lganiyu43@gmail.com>
2026-02-10 08:31:22 +0100

    Add GitHub service with fetchUserData function

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,13 +1,7 @@
 import axios from "axios";
 
 export async function fetchUserData(username) {
-  const token = import.meta.env.VITE_APP_GITHUB_API_KEY;
-
-  const response = await axios.get(`https://api.github.com/users/${username}`, {
-    headers: {
-      Authorization: `token ${token}`,
-    },
-  });
-
+  // Call the GitHub API endpoint for user data
+  const response = await axios.get(`https://api.github.com/users/${username}`);
   return response.data;
 }
