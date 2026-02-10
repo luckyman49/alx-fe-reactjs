5d1cdfd8a359d786011c41c12b6ad304826f0055
ganiyu lukman <lganiyu43@gmail.com>
2026-02-10 08:35:56 +0100

    Fix error message text and API endpoint for GitHub user search

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export async function fetchUserData(username) {
-  // Call the GitHub API endpoint for user data
   const response = await axios.get(`https://api.github.com/users/${username}`);
   return response.data;
 }
